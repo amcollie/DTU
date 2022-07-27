16d60927946b97e9b88b363fa5358b4b329dab66
Aaron Rory Newbold <aaron_newbold@hotmail.com>
2022-07-27 02:05:36 -0400

    Barcode Generator
    - added barcode.php which Contains function to generate and echo both QR and Bar code encoded with the value of a given input.
    
    - added composer.json to install required libraries for barcode and qr code generation

---
+++ PHP CODE/barcode.php
@@ -0,0 +1,34 @@
+<?php
+// declare(strict_types=1);
+
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
+require_once __DIR__.'/vendor/autoload.php';
+
+// Generates and displays Barcode, and QRCode encoded with given variable $record_id
+function GenerateImageCode($record_id) {
+    // Generate Barcode
+    $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
+    $barcode = $generator->getBarcode($record_id, $generator::TYPE_CODE_128);
+
+    // Generate QRCode
+    $options = new QROptions([
+        'eccLevel' => QRCode::ECC_L,
+        'outputType' => QRCode::OUTPUT_MARKUP_SVG,
+        'imageBase64'  => false,
+        // 'svgViewBoxSize' => 530,
+        'version' => 5
+    ]);
+    
+    $qrcode = (new QRCode($options))->render($record_id);
+
+    echo "<div style= 'max-width: 400px; text-align: center; box-shadow: 5px 10px #cccccc; padding: 8px;'>
+            <h1> $record_id </h1>
+            <div style='margin: auto; width: 50%;'> $qrcode </div><br>
+            <div style='display: flex; justify-content: center;'> $barcode </div>
+            <br>
+        </div>";
+}
+
+// GenerateImageCode('123AIJSDI9021');
+?>
