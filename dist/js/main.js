d941f77791c958d1db6d4f292312a02bd00c3a43
Allen McIntosh II <allen.mcintoshii@gmail.com>
2022-07-28 10:13:12 -0400

    Update dist

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,7 +19,7 @@
 /* 1. Loading / Opening ................ */
 /* ------------------------------------- */
 
-$(window).load(function(){
+$(function() {
     "use strict";
 
     setTimeout(function(){
@@ -35,7 +35,7 @@ $(window).load(function(){
             }
         })
 
-    },1000);
+    }, 300);
 
     setTimeout(function(){
         $('.global-overlay').velocity({
